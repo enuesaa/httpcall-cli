6ad8320122079f875eafa37b8bfbb10e020d718a
enuesaa <nsrciog@gmail.com>
2023-05-13 18:41:17 +0900

    feat: npm に publish できるようにした

--- packages/httpcall/bin/httpcall.js
+++ packages/httpcall/bin/httpcall.js
@@ -1,8 +1,10 @@
-#!/usr/bin/env node --no-warnings --loader @esbuild-kit/esm-loader
+#!/usr/bin/env node --no-warnings --experimental-specifier-resolution=node
 
 /**
  * @see https://github.com/slidevjs/slidev/blob/main/packages/slidev/bin/slidev.js
  * @see https://numb86-tech.hatenablog.com/entry/2020/06/03/203722
+ * 
+ * nodejs 19 で experimental-specifier-resolution が使えなくなるので --loader node_modules/@esbuild-kit/esm-loader に切り替えたい
  */
 
 import '../dist/cli'
