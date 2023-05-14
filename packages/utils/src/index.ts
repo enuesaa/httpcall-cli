3fe9147867c3faf0602d194c5601a4d515591852
enuesaa <nsrciog@gmail.com>
2023-05-15 01:45:48 +0900

    chore: remove memo

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -26,9 +26,3 @@ export const fetcher = async (request: FetcherRequest): Promise<FetcherResponse>
     body,
   }
 }
-
-/**
- * JSON はどこで定義されているか. v8らしい
- * https://github.com/nodejs/node/blob/2dd6d76c898c930d0f7d9386bde4b6dec37b7a49/deps/v8/src/builtins/builtins-json.cc#L32
- * https://github.com/search?q=repo%3Av8%2Fv8+JsonStringify&type=code
- */
