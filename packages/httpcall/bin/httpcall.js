6aa497c70b1cbbceff301e5c4febe6d389838b27
enuesaa <nsrciog@gmail.com>
2023-05-14 18:28:23 +0900

    chore: loader が package を見つけられない問題を調査した

--- packages/httpcall/bin/httpcall.js
+++ packages/httpcall/bin/httpcall.js
@@ -1,10 +1,10 @@
-#!/usr/bin/env node --no-warnings --experimental-specifier-resolution=node
+#!/usr/bin/env node --experimental-loader @esbuild-kit/esm-loader
 
 /**
  * @see https://github.com/slidevjs/slidev/blob/main/packages/slidev/bin/slidev.js
  * @see https://numb86-tech.hatenablog.com/entry/2020/06/03/203722
  * 
- * nodejs 19 で experimental-specifier-resolution が使えなくなるので --loader node_modules/@esbuild-kit/esm-loader に切り替えたい
+ * nodejs 19 で experimental-specifier-resolution が使えなくなるので --experimental-loader @esbuild-kit/esm-loader に切り替えたい
  */
 
 import '../dist/cli'
