05f6d2f905754f49dd0d291ab218175da4b0d481
enuesaa <nsrciog@gmail.com>
2023-05-17 23:20:22 +0900

    feat: いったん experimental-specifier-resolution フラグで実装する

--- packages/httpcall-commander/bin/httpcall.js
+++ packages/httpcall-commander/bin/httpcall.js
@@ -1,9 +1,3 @@
-#!/usr/bin/env node --no-warnings --loader @esbuild-kit/esm-loader
-
-/**
- * @see https://github.com/slidevjs/slidev/blob/main/packages/slidev/bin/slidev.js
- * @see https://numb86-tech.hatenablog.com/entry/2020/06/03/203722
- * @see https://github.com/esbuild-kit/esm-loader
- */
+#!/usr/bin/env node --no-warnings --experimental-specifier-resolution node
 
 import '../dist/cli'
